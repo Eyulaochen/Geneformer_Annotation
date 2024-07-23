0083efd9c58cf350e671f24c2307007b1422f608
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-23 16:23:19 -0500

    Update Training_Validation.py

--- Training_Validation.py
+++ Training_Validation.py
@@ -3,3 +3,12 @@ from collections import Counter
 
 full29 = sc.read_h5ad('full/HRCAv2_snRNA.h5ad', backed='r')
 print(Counter(full29.obs['celltype']))
+
+data_dict = {
+        'Chen_a_10x3_Lobe_19_D003_Nu': 'no_enriched',
+        'Chen_a_10x_Lobe_D28_13_NeuN': 'bc_enriched',
+        'BCM_22_0458_RNA_macular_NeuN': 'ac_enriched',
+        'BCM_22_0784_RNA_macular_NeuN': 'rgc_enriched',
+        'Chen_b_D001-12_lobe_NeuNM': 'amd_sample1',
+        'Chen_b_D001-12_lobe_NeuNT': 'amd_sample2'
+            }
