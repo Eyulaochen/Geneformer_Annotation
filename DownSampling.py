22f4cb2961c88b16310e7a3407356cde9ec0fc31
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-23 15:32:06 -0500

    Update DownSampling.py

--- DownSampling.py
+++ DownSampling.py
@@ -15,3 +15,22 @@ print(classes_to_downsample)
 print(len(classes_to_downsample))
 
 classes_to_downsample = [x[0] for x in classes_to_downsample]
+
+selected_indices = []
+for class_label in classes_to_downsample:
+    class_indices = np.where(train29.obs['celltype'] == class_label)[0]
+    factor = downsample/len(class_indices)
+    selected_class_indices = []
+    for sample in list(Counter(train29.obs['sampleid'])):
+        class_sample = np.where((train29.obs['celltype'] == class_label) & (train29.obs['sampleid'] == sample))[0]
+        num_samples_to_del = int(len(class_sample) * (1-factor))
+        selected_class_sample = np.random.choice(class_sample, size=num_samples_to_del, replace=False)
+        selected_class_indices.append(selected_class_sample)
+    selected_class_indices_ = np.concatenate(selected_class_indices)
+    selected_indices.append(selected_class_indices_)
+    
+all_selected_indices = np.concatenate(selected_indices)
+remaining_indices = np.setdiff1d(np.arange(len(train29)), all_selected_indices)
+downsampled_adata = train29[remaining_indices]
+
+downsampled_adata.write("downsampled372K_snrna.h5ad", compression="gzip", compression_opts=3)
