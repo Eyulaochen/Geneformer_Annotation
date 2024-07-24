780ef7354546d354ac322a344e9bda0bf2a14c02
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-24 15:05:40 -0500

    Update and rename Fine_Tuning to Fine_Tuning.py

---
+++ Fine_Tuning.py
@@ -0,0 +1,47 @@
+
+from transformers import BertForSequenceClassification
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def classes_to_ids(example):
+    example["label"] = target_name_id_dict[example["label"]]
+    return example
+evalset = ds.map(classes_to_ids, num_proc=16)
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    # calculate accuracy and macro f1 using sklearn's function
+    acc = accuracy_score(labels, preds)
+    macro_f1 = f1_score(labels, preds, average='macro')
+    return {
+      'accuracy': acc,
+      'macro_f1': macro_f1
+    }
+
+def FineTuning(training_data_dir, output_dir):
+    # set model parameters
+    # max input size
+    max_input_size = 2**11  # 2048
+    # set training hyperparameters
+    # max learning rate
+    max_lr = 5e-5
+    # how many pretrained layers to freeze
+    freeze_layers = 0
+    # number gpus
+    num_gpus = 1
+    # number cpu cores
+    num_proc = 16
+    # batch size for training and eval
+    geneformer_batch_size = 12
+    # learning schedule
+    lr_schedule_fn = 'linear'
+    # warmup steps
+    warmup_steps = 500
+    # number of epochs
+    epochs = 10
+    # optimizer
+    optimizer = 'adamw'
+
+    
+    
