0104509be835f70d00a9e88575250d5829624c25
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-24 17:00:38 -0500

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -9,6 +9,11 @@ from collections import Counter
 from datasets import load_from_disk
 from datasets import Dataset
 from geneformer.pretrainer import token_dictionary
+with open('gene_name_id_dict.pkl', 'rb') as f:
+    names = pickle.load(f)
+with open('gene_median_dictionary.pkl', 'rb') as f:
+    median = pickle.load(f)
+
 
 target_name_id_dict = np.load('2_9M_111celltype.npy',allow_pickle='TRUE').item()
 
