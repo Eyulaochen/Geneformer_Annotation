d2f96b98a20e770ee43cc9ac3d1b7a966fde5e01
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-24 14:27:19 -0500

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -1,5 +1,16 @@
+import pickle
 import scanpy as sc
 import numpy as np
+from collections import Counter
+from datasets import load_from_disk
 from geneformer.pretrainer import token_dictionary
+with open('gene_name_id_dict.pkl', 'rb') as f:
+    names = pickle.load(f)
+with open('gene_median_dictionary.pkl', 'rb') as f:
+    median = pickle.load(f)
 
-data = 
+data = sc.read_h5ad('full/VAL_snRNA2_9M.h5ad', backed='r')
+X = data.X[:]
+X = X.toarray()
+var = data.var.index
+
