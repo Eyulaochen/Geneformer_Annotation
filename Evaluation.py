2888c0cb817238585c08f2ee14dd29677c22a1ed
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-24 16:59:07 -0500

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -1,1 +1,14 @@
+import os
+import scanpy 
+import torch
+import pickle
+import numpy as np
+from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+from collections import Counter
+from datasets import load_from_disk
+from datasets import Dataset
+from geneformer.pretrainer import token_dictionary
+
+target_name_id_dict = np.load('2_9M_111celltype.npy',allow_pickle='TRUE').item()
 
