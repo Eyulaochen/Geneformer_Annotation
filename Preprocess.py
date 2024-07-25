55748c0fd3eb35b8b6840cc489bbb52a7849584a
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-25 15:36:00 -0500

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -55,8 +55,8 @@ def process(data_dir):
     ds = Dataset.from_dict(data_dict)
     ds.save_to_disk(data_dir[:-5] + '.dataset')
 
-data_dir = 'data/VAL_snRNA2_9M.h5ad'
-process(data_dir)
+#data_dir = 'data/VAL_snRNA2_9M.h5ad'
+#process(data_dir)
 
-test = load_from_disk(data_dir[:-5] + '.dataset')
-print(test[0])
+#test = load_from_disk(data_dir[:-5] + '.dataset')
+#print(test[0])
