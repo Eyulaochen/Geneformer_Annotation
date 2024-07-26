d976e4c640c068273f4f292115ed63722bf2200b
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-26 17:25:04 -0500

    Update Numerical_Labels.py

--- Numerical_Labels.py
+++ Numerical_Labels.py
@@ -1,1 +1,9 @@
 
+
+# prepare the numerical labels
+training = sc.read_h5ad('data/downsampled372K_snrna.h5ad', backed='r')
+target_names = list(Counter(training.obs['celltype']))
+target_name_id_dict = dict(zip(target_names,[i for i in range(len(target_names))]))
+np.save('2_9M_111celltype.npy', target_name_id_dict)
+#target_name_id_dict = np.load('2_9M_111celltype.npy',allow_pickle='TRUE').item()
+del training
