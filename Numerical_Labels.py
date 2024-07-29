bbf8b73c9249540c1bd71b373a5028b835d5151c
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-29 18:37:58 -0500

    Update Numerical_Labels.py

--- Numerical_Labels.py
+++ Numerical_Labels.py
@@ -1,9 +1,10 @@
+import sys
 import scanpy as sc
 import numpy as np
 from collections import Counter
 
 # prepare the numerical labels
-training = sc.read_h5ad('data/downsampled372K_snrna.h5ad', backed='r')
+training = sc.read_h5ad(sys.argv[1], backed='r')
 target_names = list(Counter(training.obs['celltype']))
 target_name_id_dict = dict(zip(target_names,[i for i in range(len(target_names))]))
 np.save('2_9M_111celltype.npy', target_name_id_dict)
