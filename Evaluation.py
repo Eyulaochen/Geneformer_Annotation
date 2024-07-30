4f4877f492ac79496325d9efe840f68ae9533633
Eyulaochen <112418288+Eyulaochen@users.noreply.github.com>
2024-07-30 14:17:24 -0500

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -10,11 +10,56 @@ from collections import Counter
 from datasets import load_from_disk
 from datasets import Dataset
 from geneformer.pretrainer import token_dictionary
-from Preprocess import process
+#from Preprocess import process
 with open('gene_name_id_dict.pkl', 'rb') as f:
     names = pickle.load(f)
 with open('gene_median_dictionary.pkl', 'rb') as f:
     median = pickle.load(f)
+
+def process(data_dir):
+    data = sc.read_h5ad(data_dir, backed='r')
+    X = data.X[:]
+    X = X.toarray()
+    var = data.var.index
+
+    l = []
+    indvec = []
+    ct = 0
+    for v in var:
+        if (v not in names.keys()) or (names[v] not in token_dictionary.keys()):
+            l.append(ct)
+            ct = ct+1
+        else:
+            indvec.append(token_dictionary[names[v]])
+            X[:,ct] /=(median[names[v]])
+            ct = ct+1
+    X = np.delete(X, l, 1)
+    X.shape
+    
+    data_list = []
+    for i in range(len(X)):
+        argsort = np.argsort(X[i,:])
+        c = len(argsort) - np.count_nonzero(X[i,:])
+        data_list.append(np.array(indvec)[argsort[c:].astype(int)])
+
+    # cut > 2048
+    for i in range(len(data_list)):
+        # reverse order
+        data_list[i] = np.flip(data_list[i])
+        if (len(data_list[i]) > 2048):
+            data_list[i] = data_list[i][:2048]
+
+    data_list_ = []
+    for i in range(len(data_list)):
+        data_list_.append(data_list[i].copy().tolist())
+
+    lengths = []
+    for i in range(len(data_list_)):
+        lengths.append(len(data_list_[i]))
+    
+    data_dict = {"label": data.obs['celltype'], "input_ids": data_list_, "length": lengths}
+    ds = Dataset.from_dict(data_dict)
+    ds.save_to_disk(data_dir[:-5] + '.dataset')
 
 target_name_id_dict = np.load('2_9M_111celltype.npy',allow_pickle='TRUE').item()
 
